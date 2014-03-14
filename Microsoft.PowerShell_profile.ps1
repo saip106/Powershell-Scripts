2586b0ba9b3a2121016604745afb77afb07eee68
Sai Gudigundla <saip106@msn.com>
2014-03-14 17:45:50 -0400

    modified some shortcuts and added sublime

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,10 +2,13 @@ Set-Location C:\projects\sandbox
 
 # Set all the aliases here
 Set-Alias which Get-Command
+Set-Alias c Clear-Host
 
-Set-Alias lime "C:\Program Files\Sublime Text 2\sublime_text.exe"
-Set-Alias web "C:\Program Files (x86)\JetBrains\WebStorm 5.0.4\bin\webstorm.exe"
-Set-Alias note "C:\Program Files\Notepad2\notepad2.exe"
+Set-Alias w "C:\Program Files (x86)\JetBrains\WebStorm 7.0.3\bin\WebStorm.exe"
+Set-Alias n "C:\Program Files\Notepad2\notepad2.exe"
+Set-Alias note++ "C:\Program Files (x86)\Notepad++\notepad++.exe"
+Set-Alias n++ "C:\Program Files (x86)\Notepad++\notepad++.exe"
+Set-Alias s "C:\Program Files\Sublime Text 3\sublime_text.exe"
 
 function cd_to_sandbox { set-location "C:\projects\sandbox" }
 Set-Alias sand cd_to_sandbox
@@ -19,13 +22,13 @@ Set-Alias desk cd_to_desktop
 function touch {set-content -Path ($args[0]) -Value ($null) }
 
 function gcap($message) {
-  git add .;
+  git add --all;
   git commit -m $message;
   git push origin;
 }
 
 function gac($message) {
-  git add .;
+  git add --all;
   git commit -m $message;
 }
 
